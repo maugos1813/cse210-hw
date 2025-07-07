1d0dde8baab76418e5b3ad7ae78bbbb31dc899c6
maugos1813 <maufabagosgam@gmail.com>
2025-07-07 16:49:47 -0500

    prep 3 finish

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess (1-100)? ");
+            guess = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Debug: magic={magicNumber}, guess={guess}");
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("The number is higher than your guess.");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("The number is lower than your guess.");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
